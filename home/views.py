7cc10a38241d9409315b741ae38fac68c4681aaf
junanote <tjfduddl00@naver.com>
2021-07-14 08:50:59 +0900

    http response 적용

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    path = request.path
+    resultstr = ''
+    if path == '/home':
+        resultstr = '<h1>여기는 home입니다.</h1>'
+    else :
+        resultstr = '<h1>여기는 main입니다.</h1>'
+
+    return HttpResponse(resultstr)
+
+def index01(request):
+    result = {'first':'sanghun','second':'Oh'}
+    return render(request, 'index.html', context=result)
+
+def index02(request):
+    request.GET['first']
+    result = {'first':request.GET['first'],'second':request.GET['second']}
+    return render(request, 'index.html', context=result)
+
