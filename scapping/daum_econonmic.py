a79b2ba1454d4bfd9850127cbe57eec4d7048962
junanote <tjfduddl00@naver.com>
2021-07-15 11:29:54 +0900

    sql 2 적용

--- scapping/daum_econonmic.py
+++ scapping/daum_econonmic.py
@@ -7,12 +7,17 @@ import sqlite3
 if res.status_code == 200:
     soup = BeautifulSoup(res.content, 'html.parser')
     links = soup.select('a.link_txt')
+    connect = sqlite3.connect('./db.sqlite3')
+    cursor = connect.cursor()
+
     for link in links:
         title = str.strip(link.get_text())
         href = str.strip(link.get('href'))
         print(title, ' : ', href)
-        connect = sqlite3.connect('./db.sqlite3')
-        cursor = connect.cursor()
-        cursor.execute("insert into polls_economics(create_date, href, title) values(datetime('now'), "+href+", "+title+")")
-
+        try:
+            cursor.execute(
+                "insert into polls_economics(create_date, href, title) values(datetime('now'),?,?)", (href, title))
+        except:
+            pass
+        print(title, '  :  ', href)
     connect.commit()
